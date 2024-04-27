98085cf91b5acc54c26a2fea34d593f1c637d40b
psa90911017 <psa90911017@gmail.com>
2024-04-27 15:48:17 +0900

    db.json(하단에데이터입력) / App.js(dessert 경로설정) / Navbar.js(dessert  오타수정) / index.js(dessertReducer 추가) / public-images 파일추가(30) / DessertAPICalls.js 파일추가 / DessertModifyForm.js 파일추가 / DessertRegistForm.js 파일추가 / Dessert.js 파일추가 / DessertItem.js 파일추가 / DessertList.js 파일추가 / DessertModule.js 파일추가 / DessertDetail.js 파일추가 / DessertMenus.js 파일추가 / DessertModify.js 파일추가 / DessertRegist.js 파일추가 / 전체메뉴조회 기능구현완료 / 상세메뉴조회 기능구현완료

--- MOONBUCKS/src/App.js
+++ MOONBUCKS/src/App.js
@@ -7,6 +7,10 @@ import MenuRegist from './pages/MenuRegist';
 import MenuModify from './pages/MenuModify';
 import Login from './pages/Login';
 import Error from './pages/Error';
+import DessertMenus from './pages/DessertMenus';
+import DessertDetail from './pages/DessertDetail';
+import DessertRegist from './pages/DessertRegist';
+import DessertModify from './pages/DessertModify';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -36,6 +40,14 @@ function App() {
 					</Route>
 					<Route path="login" element={<Login />} />
 					<Route path="main" element={<Main />} />
+					<Route path="dessert" >
+						<Route index element={<DessertMenus />} />
+						<Route path=":id" element={<DessertDetail />} />
+						<Route path="regist" element={<DessertRegist />} />
+						<Route path="modify" >
+							<Route path=":id" element={<DessertModify />} />
+						</Route>
+					</Route>
 				</Route>
 				<Route path="*" element={<Error />} />
 			</Routes>
