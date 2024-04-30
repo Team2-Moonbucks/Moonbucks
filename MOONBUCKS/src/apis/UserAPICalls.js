af6c012a14a26b2f04e79f58c92d54562a283b3d
jaeghong011 <jaeghong011@gmail.com>
2024-04-30 11:31:04 +0900

    docs: user api, module 수정 및 추가
    회원 전체조회, 상세조회, 등록, 삭제 기능 api, reducer 추가

--- MOONBUCKS/src/apis/UserAPICalls.js
+++ MOONBUCKS/src/apis/UserAPICalls.js
@@ -1,5 +1,5 @@
 import { request } from "./Api";
-import { login } from "../modules/UserModule";
+import { login, getUserlist, getUser, registUser, modifyUser, deleteUser } from "../modules/UserModule";
 
 /* 로그인 정보 전달 받는 함수 */
 export function callLoginAPI(loginInfo) {
@@ -21,5 +21,73 @@ export function callLoginAPI(loginInfo) {
 		/* action 생성 함수에 결과 전달하며 dispatch 호출 */
 		dispatch(login(result));
 
+	}
+}
+
+export function callGetUserListAPI() {
+
+	console.log('getUserList api calls...');
+
+	/* redux-thunk(미들 웨어)를 이용한 비동기 처리 */
+	return async (dispatch, getState) => {
+
+		/* Api의 axios 처리 참조  */
+		const result = await request('GET', '/user');
+		console.log('getUserList result : ', result);
+
+		/* action 생성 함수에 결과 전달하며 dispatch 호출 */
+		dispatch(getUserlist(result));
+	}
+}
+
+export function callGetUserAPI(id) {
+
+	console.log('getUser api calls...');
+
+	return async (dispatch, getState) => {
+
+		const result = await request('GET', `/user/${id}`);
+		console.log('getUser result : ', result);
+
+		dispatch(getUser(result));
+	}
+}
+
+export function callRegistUserAPI(user) {
+
+	console.log('registUser api calls...');
+
+	return async (dispatch, getState) => {
+
+		const result = await request('POST', '/user/', user);
+		console.log('registUser result : ', result);
+
+		dispatch(registUser(result));
+	}
+}
+
+export function callModifyUserAPI(user) {
+
+	console.log('modifyUser api calls...');
+
+	return async (dispatch, getState) => {
+
+		const result = await request('PUT', `/user/${user.id}`, user);
+		console.log('modifyUser result : ', result);
+
+		dispatch(modifyUser(result));
+	}
+}
+
+export function callDeleteUserAPI(id) {
+
+	console.log('deleteUser api calls...');
+
+	return async (dispatch, getState) => {
+
+		const result = await request('DELETE', `/user/${id}`);
+		console.log('deleteUser result : ', result);
+
+		dispatch(deleteUser(result));
 	}
 }
